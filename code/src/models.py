acb01d886393cf2bfee89e54d6449b3ad071c5f6
Lucas Brito <luzabri7@gmail.com>
2024-05-01 14:21:17 -0400

    begin implementing equivariant

--- code/src/models.py
+++ code/src/models.py
@@ -3,31 +3,56 @@ TODO
 - Equivariant layer 
 - invariant layer 
 - Gauge equivariant NN 
+*** ALL WEIGHTS SHOULD BE REAL VALUED ***
 """
 
 import flax.linen as nn 
 import jax.numpy as np
+from wilson import * 
 
 
 class EquivariantCNN(nn.Module): 
+    # include parameters like hidden size here: 
+    graph = None
+    kernel_size = (3, 3)
 
-	# compact lets you define all the layers IN call as opposed to in init like 
-	# in tensorflow. I can imagine that for skip connections this might be bad. 
-	@nn.compact 
-	def __call__(self, x): 
-		# TODO 
-		pass 
+    # compact lets you define all the layers IN call as opposed to in init like 
+    # in tensorflow. I can imagine that for skip connections this might be bad. 
+    @nn.compact 
+    def __call__(self, x): 
+        # Input should be of shape (B, N); B = batch size, N = number of dofs
+
+        w_loops = wilson_loops(x, self.graph)
+        w_lines = wilson_lines(x, self.graph)
+
+        w_loops = nn.Conv(features=2, # for vertical and horizontal loops             
+                          kernel_size=self.kernel_size, 
+                          strides=1, 
+                          padding='CIRCULAR')(w_loops) # PBC
+
+        w_loops = nn.activation.leaky_relu(w_loops)
+
+        # TODO reshape w_loops. w_lines is [vertical_edges, horizontal_edges]
+        pointwise = w_lines * w_loops
+
+        return x + pointwise
 
 class InvariantCNN(nn.Module): 
 
-	@nn.compact 
-	def __call__(self, x): 
-		# TODO 
-		pass 
+    @nn.compact 
+    def __call__(self, x): 
+        """
+        w_loops = wilson_loops(x)
+        cnn1 = conv(w_loops) 
+        cnn2 = conv(w_loops) 
+        out1 = log(abs(elu(avg_pool(cnn1))))
+        out2 = arg(softsign(avg_pool(cnn2)))/pi
+        return out1, out2 (actually, 75% sure you just add them together)
+        """
 
 class GENN(nn.Module): 
-	@nn.compact 
-	def __call__(self, x): 
-		# TODO 
-		pass 
+    @nn.compact 
+    def __call__(self, x): 
+        # TODO 
+        pass 
 
--- code/src/wilson.py
+++ code/src/wilson.py
@@ -2,4 +2,44 @@
 TODO 
 - wilson loops 
 - wilson lines ⊐, ⊔ (missing edge is argument) 
+
+should try to use flax so that it is differentiable
 """
+
+import flax.linen as nn 
+import jax.numpy as np
+
+def ordered_edges(graph): 
+    """
+    Assumes square LxL lattice.
+    """
+    a = [] 
+    L = np.sqrt(graph.n_nodes)
+    for x in graph.nodes(): 
+        a.append((x, int((x + L) % graph.n_nodes)))
+    
+    for x in graph.nodes(): 
+        a.append((x, int(L * (x // L) + (x + 1) % L)))
+    
+    return a
+
+def wilson_loop(i, x, graph): 
+    """
+    There is an unique Wilson loop for each lattice site. `i` indexes site for 
+    this Wilson loop.
+    """
+    L = int(np.sqrt(graph.n_nodes))
+    left = x[i] 
+    right = x[i + 1]
+    upper = x[i + L**2]
+    lower = x[i + L**2 + L]
+
+
+    pass
+
+
+def wilson_loops(x, graph): 
+    pass
+
+def wilson_lines(x, graph): 
+    pass 
