5a43a3a40d82fb2b890800beb103b346acc7ea76
“areme21” <adamremels1@gmail.com>
2024-05-01 17:52:06 -0400

    architecture outline

--- code/src/models.py
+++ code/src/models.py
@@ -19,22 +19,20 @@ class EquivariantCNN(nn.Module):
     # compact lets you define all the layers IN call as opposed to in init like 
     # in tensorflow. I can imagine that for skip connections this might be bad. 
     @nn.compact 
-    def __call__(self, x): 
+    def __call__(self, loops, lines): 
         # Input should be of shape (B, N); B = batch size, N = number of dofs
-        x = ordered_edges(x)
-        w_loops, w_lines_left, w_lines_up = get_wilson_loops_and_lines(x, np.sqrt(x.n_nodes))
 
         w_loops = nn.Conv(features=2, # for vertical and horizontal loops             
                           kernel_size=self.kernel_size, 
                           strides=1, 
-                          padding='CIRCULAR')(w_loops) # PBC
+                          padding='CIRCULAR')(loops) # PBC
 
         w_loops = nn.activation.leaky_relu(w_loops)
 
         # TODO reshape w_loops. w_lines is [vertical_edges, horizontal_edges]
-        pointwise = (w_lines_left + w_lines_up) * w_loops
+        pointwise = lines * w_loops
 
-        return x + pointwise
+        return pointwise
 
 class InvariantCNN(nn.Module): 
     kernel_size = (3, 3)
@@ -49,11 +47,10 @@ class InvariantCNN(nn.Module):
         out2 = arg(softsign(avg_pool(cnn2)))/pi
         return out1, out2 (actually, 75% sure you just add them together)
         """
-        w_loops, _, _ = get_wilson_loops_and_lines(x)
         w_loops = nn.Conv(features=2, # for vertical and horizontal loops             
                           kernel_size=self.kernel_size, 
                           strides=1, 
-                          padding='CIRCULAR')(w_loops) 
+                          padding='CIRCULAR')(x) 
         w_loops = nn.activation.leaky_relu(w_loops)
         out1 = np.log(np.abs(nn.elu(nn.avg_pool(w_loops))))
         out2 = nn.soft_sign(nn.avg_pool(w_loops)) / np.pi
@@ -64,5 +61,11 @@ class GENN(nn.Module):
     @nn.compact 
     def __call__(self, x): 
         # TODO 
-        pass 
+        x = ordered_edges(x)
+        w_loops, w_lines_left, w_lines_up = get_wilson_loops_and_lines(x, np.sqrt(x.n_nodes))
+        eq = EquivariantCNN()(w_loops, w_lines_left + w_lines_up)
+        skip = x + eq
+        out = InvariantCNN()(skip)
+        return out
+
 
