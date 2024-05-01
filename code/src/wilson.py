6160296dc9a6c6eed6af2ba80c33155159cd5c03
Lucas Brito <luzabri7@gmail.com>
2024-05-01 17:14:43 -0400

    fix wilson loops

--- code/src/wilson.py
+++ code/src/wilson.py
@@ -23,7 +23,7 @@ def ordered_edges(graph):
     
     return a
 
-def get_wilson_loops_and_lines(edges, L):
+def get_wilson_loops_and_lines(x, graph):
     """
     Given a list of edges, returns the wilson loops and paths for every vertex in the original lattice.
     Assumes a square lattice of size (L x L) with periodic boundary conditions, the input should be the output of ordered_edges(nk.graph.Square(L, pbc=True))
@@ -38,20 +38,18 @@ def get_wilson_loops_and_lines(edges, L):
     wilson_loops = []
     wilson_paths_left = []
     wilson_paths_up = []
+
+    L = np.sqrt(graph.n_nodes)
+
     for i in range(L**2):
-        left = ord_edges[i] 
-        right = ord_edges[i + 1]
-        upper = ord_edges[i + L**2]
-        lower = ord_edges[(i + L**2 + L) % (L**2)] # <--- this goes out of range
+        left  = x[i] 
+        right = x[int(L * (i // L) + (i + 1) % L)]
+        upper = x[i + L**2]
+        lower = x[(i + L) % (L**2) + L**2]
 
-        S_left = get_score(left)
-        S_right = get_score(right)
-        S_upper = get_score(upper)
-        S_lower = get_score(lower)
-
-        wilson_loops.append(S_left * S_right * S_lower * S_upper)
-        wilson_paths_left.append(S_left * S_lower * S_right)
-        wilson_paths_up.append(S_upper * S_right * S_lower)
+        wilson_loops.append(left * right * lower * upper)
+        wilson_paths_left.append(left * lower * right)
+        wilson_paths_up.append(upper * right * lower)
 
     return wilson_loops, wilson_paths_left, wilson_paths_up
 
