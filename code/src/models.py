5f9607df0e20eeff0c3166bf79f1ab482fd6939a
Lucas Brito <luzabri7@gmail.com>
2024-05-01 23:36:42 -0400

    debug genn

--- code/src/models.py
+++ code/src/models.py
@@ -8,8 +8,9 @@ TODO
 
 import flax.linen as nn 
 import jax.numpy as np
-from wilson import * 
-
+import netket as nk
+from src.wilson import * 
+import math
 
 class EquivariantCNN(nn.Module): 
     # include parameters like hidden size here: 
@@ -19,6 +20,7 @@ class EquivariantCNN(nn.Module):
     def __call__(self, loops, lines): 
         # Input should be of shape (B, N); B = batch size, N = number of dofs
 
+        loops = np.expand_dims(loops, -1)
         w_loops = nn.Conv(features=2, # for vertical and horizontal loops             
                           kernel_size=self.kernel_size, 
                           strides=1, 
@@ -33,6 +35,7 @@ class EquivariantCNN(nn.Module):
 
 class InvariantCNN(nn.Module): 
     kernel_size = (3, 3)
+    L: int = None
 
     @nn.compact 
     def __call__(self, x): 
@@ -44,32 +47,58 @@ class InvariantCNN(nn.Module):
         out2 = arg(softsign(avg_pool(cnn2)))/pi
         return out1, out2 (actually, 75% sure you just add them together)
         """
-        w_loops = nn.Conv(features=2, # for vertical and horizontal loops             
+        w_loops = nn.Conv(features=2, 
                           kernel_size=self.kernel_size, 
                           strides=1, 
                           padding='CIRCULAR')(x) 
         w_loops = nn.activation.leaky_relu(w_loops)
-        out1 = np.log(np.abs(nn.elu(nn.avg_pool(w_loops))))
-        out2 = nn.soft_sign(nn.avg_pool(w_loops)) / np.pi
-        return out1 + 1j * out2
+        cnn = nn.avg_pool(w_loops, window_shape=(self.L, self.L)) # out is (B, 1, 1, 2)
+        cnn = cnn.reshape(-1, 2)
+        cnn1 = np.log(cnn[0])
+        cnn2 = np.pi * nn.soft_sign(cnn[1])
+        # out1 = np.log(np.abs(nn.elu(nn.avg_pool(w_loops, window_shape=(self.L, self.L)))))
+        # out2 = np.pi * nn.soft_sign(nn.avg_pool(w_loops, window_shape=(self.L, self.L))),
+        return cnn1 + 1j * cnn2
 
 
 class GENN(nn.Module): 
-    graph = None
+
+    graph: nk.graph.Graph = None
+
+    def setup(self): 
+        self.L = int(math.sqrt(self.graph.n_nodes))
+
     @nn.compact 
     def __call__(self, x): 
-        # TODO 
-        L = np.sqrt(self.graph.n_nodes)
-        x = ordered_edges(x)
-        w_loops, w_lines_left, w_lines_up = get_wilson_loops_and_lines(x, np.sqrt(x.n_nodes))
-        w_loops = w_loops.reshape((L, L))
-        w_lines_left = w_lines_left.reshape((L, L))
-        w_lines_up = w_lines_up.reshape((L, L))
-        print(type(w_lines_up))
-        lines = [w_lines_left, w_lines_up] # convert to flax tensor type
+        w_loops, w_lines_left, w_lines_up = get_wilson_loops_and_lines(x, self.L)
+
+        w_loops = w_loops.reshape(-1, self.L, self.L)
+        w_lines_left = w_lines_left.reshape(-1, self.L, self.L)
+        w_lines_up = w_lines_up.reshape(-1, self.L, self.L)
+
+        lines = np.append(np.expand_dims(w_lines_left, -1), 
+                          np.expand_dims(w_lines_up, -1),
+                          axis=-1)
+
         eq = EquivariantCNN()(w_loops, lines)
-        skip = x + eq
-        out = InvariantCNN()(skip)
+        
+        # x_ver = x[:, :self.L**2]
+        # x_hor = x[:, self.L**2:]
+        
+        # x_ver = x_ver.reshape(-1, self.L, self.L)
+        # x_hor = x_hor.reshape(-1, self.L, self.L)
+        
+        # x_reshaped = np.append(np.expand_dims(x_ver, -1), 
+        #                        np.expand_dims(x_hor, -1),
+        #                        axis=-1)
+
+        # skip = x_reshaped + eq
+        skip = x + eq.reshape(-1, 2 * self.L**2)
+        w_loops, _, _  = get_wilson_loops_and_lines(skip, self.L)
+        w_loops = np.expand_dims(w_loops.reshape(-1, self.L, self.L), -1)
+        print(w_loops.shape)
+        out = InvariantCNN(L=self.L)(w_loops)
+
         return out
 
 
--- code/src/wilson.py
+++ code/src/wilson.py
@@ -6,7 +6,6 @@ TODO
 should try to use flax so that it is differentiable
 """
 
-import flax.linen as nn 
 import jax.numpy as np
 
 def ordered_edges(graph): 
@@ -23,7 +22,15 @@ def ordered_edges(graph):
     
     return a
 
-def get_wilson_loops_and_lines(x, graph):
+def plaquette(i, x, L): 
+    left  = x[i] 
+    right = x[int(L * (i // L) + (i + 1) % L)]
+    upper = x[i + L**2]
+    lower = x[(i + L) % (L**2) + L**2]
+    return left, right, upper, lower
+
+
+def get_wilson_loops_and_lines(x, L):
     """
     Given a list of edges, returns the wilson loops and paths for every vertex in the original lattice.
     Assumes a square lattice of size (L x L) with periodic boundary conditions, the input should be the output of ordered_edges(nk.graph.Square(L, pbc=True))
@@ -31,25 +38,20 @@ def get_wilson_loops_and_lines(x, graph):
     - edges: an ordered list of edges, where each edge is a tuple of the form (x,y) representing the connection between two nodes
     - L: the dimension of the lattice 
     Outputs:
-    -wilson_loops: the wilson loops for each point in the graph, which is left_edge * right_edge * upper_edge * lower_edge
-    -wilson_paths_left: the wilson paths for each point in the graph, when starting by going left, which is left_edge * lower_edge * right_edge
-    -wilson_paths_up: the wilson paths for each point in the graph, when starting by going up, which is upper_edge * right_edge * lower_edge
+    - wilson_loops: the wilson loops for each point in the graph, which is left_edge * right_edge * upper_edge * lower_edge
+    - wilson_paths_left: the wilson paths for each point in the graph, when starting by going left, which is left_edge * lower_edge * right_edge
+    - wilson_paths_up: the wilson paths for each point in the graph, when starting by going up, which is upper_edge * right_edge * lower_edge
     """
-    wilson_loops = []
+    wilson_loops      = []
     wilson_paths_left = []
-    wilson_paths_up = []
-
-    L = np.sqrt(graph.n_nodes)
+    wilson_paths_up   = []
 
     for i in range(L**2):
-        left  = x[i] 
-        right = x[int(L * (i // L) + (i + 1) % L)]
-        upper = x[i + L**2]
-        lower = x[(i + L) % (L**2) + L**2]
+        left, right, upper, lower = plaquette(i, x, L)
 
-        wilson_loops.append(left * right * lower * upper)
-        wilson_paths_left.append(left * lower * right)
-        wilson_paths_up.append(upper * right * lower)
+        wilson_loops.append(left * right * lower * upper) # feels like there should be more efficient way instead of appending
+        wilson_paths_up.append(left * lower * right)
+        wilson_paths_left.append(upper * right * upper)
 
-    return wilson_loops, wilson_paths_left, wilson_paths_up
+    return np.array(wilson_loops).T, np.array(wilson_paths_left).T, np.array(wilson_paths_up).T
 
